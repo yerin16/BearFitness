bca8ba4fc84553c6543bc7a1c25a983137250bc1
Yerin Kang <61400861+yerin16@users.noreply.github.com>
2026-03-27 16:16:53 -0500

    Initial Commit

---
+++ BearFitness/BearFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  BearFitnessApp.swift
+//  BearFitness
+//
+//  Created by Yerin Kang on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct BearFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
